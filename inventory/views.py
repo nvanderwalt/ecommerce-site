5781810b9b635da6b473adbe570386591bc35b01
nvanderwalt <nvanderwalt12@yahoo.com>
2025-05-09 17:48:01 +0200

    Add product listing page with images

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'inventory/product_list.html', {'products': products})
