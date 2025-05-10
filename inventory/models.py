b6d5e464e10d5ac9124f0c5e57375d8057f8190e
nvanderwalt <nvanderwalt12@yahoo.com>
2025-05-10 14:01:21 +0200

    Add UserProfile model with fitness fields and admin support

--- inventory/models.py
+++ inventory/models.py
@@ -1,12 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
-    in_stock = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    goal = models.CharField(max_length=255, blank=True)
+    fitness_level = models.CharField(max_length=50, blank=True)
+    dietary_preference = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.userprofile.save()
