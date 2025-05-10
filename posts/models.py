e8e46378504d1bc7e8402298183aa51b06e1d783
nvanderwalt <nvanderwalt12@yahoo.com>
2025-05-10 17:28:32 +0200

    Add Post model and run initial migration

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author.username} – {self.created_at.strftime('%Y-%m-%d %H:%M')}"
