5ef134c6a0772d9213fb1aac749e532986053712
nvanderwalt <nvanderwalt12@yahoo.com>
2025-05-10 19:16:05 +0200

    Add community post views, URLs, and PostForm

---
+++ posts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(
+                attrs={
+                    'rows': 4,
+                    'placeholder': 'Share your progress…'
+                }
+            ),
+        }
