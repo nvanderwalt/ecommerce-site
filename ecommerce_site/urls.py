5885900e10b0ff4b06f22568734e3813279a9bc4
nvanderwalt <nvanderwalt12@yahoo.com>
2025-05-10 12:24:11 +0200

    Fix login redirect and cleanup duplicate settings.py

--- ecommerce_site/urls.py
+++ ecommerce_site/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from inventory.views import product_list, add_to_cart, cart_view
+from inventory.views import register
+
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,6 +12,8 @@ urlpatterns = [
     path('', product_list, name='product_list'),
     path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('cart/', cart_view, name='cart'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
 ]
 
 if settings.DEBUG:
--- inventory/views.py
+++ inventory/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
+# Product list
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'inventory/product_list.html', {'products': products})
 
+# Add product to cart
 def add_to_cart(request, product_id):
     cart = request.session.get('cart', {})
     cart[str(product_id)] = cart.get(str(product_id), 0) + 1
     request.session['cart'] = cart
     return redirect('product_list')
 
+# View cart contents
 def cart_view(request):
     cart = request.session.get('cart', {})
     cart_items = []
@@ -34,3 +39,15 @@ def cart_view(request):
         'total': total,
     }
     return render(request, 'inventory/cart.html', context)
+
+# Register new user
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('product_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
