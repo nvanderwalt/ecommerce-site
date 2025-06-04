2bdfc2c7e87f2c9a9a5fe996936410ce238fcf74
nvanderwalt <nvanderwalt12@yahoo.com>
2025-06-04 12:22:56 +0200

    Remove Cloudinary configuration

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -30,8 +30,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
-    'cloudinary_storage',
-    'cloudinary',
     
     # Local apps
     'inventory',
